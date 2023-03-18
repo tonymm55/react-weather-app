412599ae5397b12292300cbb264d9f2556684784
TonyMM55 <tonymmoran@gmail.com>
2023-03-18 15:19:26 +0000

    Rewrote test to include My Weather Application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Prop">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Weather App Header from App.js</h1>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Weather Application      
         </a>
       </header>
     </div>
