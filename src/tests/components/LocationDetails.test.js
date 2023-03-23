0a8bb55d070b6c6e2ae5e72af629d29f0c41eb59
TonyMM55 <tonymmoran@gmail.com>
2023-03-23 18:41:21 +0000

    added 2 test files

--- src/tests/components/LocationDetails.test.js
+++ src/tests/components/LocationDetails.test.js
@@ -1,3 +1,11 @@
-import React from 'react';
+import React from "react";
 import { render } from "@testing-library/react";
-import LocationDetails from "../components/LocationDetails";
+import LocationDetails from "../../components/LocationDetails";
+
+describe("LocationDetails", () => {
+  it("renders the correct city and location properties", () => {
+    const { getByText } = render(
+      <LocationDetails city="Manchester" country="UK" />
+    );
+  });
+});
