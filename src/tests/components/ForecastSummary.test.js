1dbcd22aded4a9e05e2886ad45cfa1c0e5e00986
TonyMM55 <tonymmoran@gmail.com>
2023-03-23 21:06:17 +0000

    added test file and snapshot

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { render } from "@testing-library/react";
+import ForecastSummary from "../../components/ForecastSummary";
 
 describe("ForecastSummary", () => {
-  it("renders", () => {});
+  const validProps = {
+    date: 1111111,
+    description: "Stub description",
+    icon: "stubIcon",
+    temperature: [], // temperature not as written, needs to be checked
+  };
+
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
